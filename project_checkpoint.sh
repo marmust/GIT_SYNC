ae58a8ab4999b7deab43cfeccbab095954092cca
Ubuntu <ubuntu@ip-10-0-1-20.ec2.internal>
2024-09-09 11:16:00 +0000

    automated project checkpoint backup commit

--- project_checkpoint.sh
+++ project_checkpoint.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-while true; do
-  # Get the current timestamp
-  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+git add .
+git commit -m "automated project checkpoint backup commit"
+git push
   
-  # Add and commit changes with the timestamp in the commit message
-  git add .
-  git commit -m "Automated project checkpoint commit at $timestamp"
-  git push
-  
-  # Echo a message with the timestamp
-  echo "### ### ### ### ### ### ### ### ### ### ### ###   Checkpoint finished at $timestamp   ### ### ### ### ### ### ### ### ### ### ### ###"
-  
-  # Wait for 10 minutes (600 seconds) before repeating
-  sleep 600
+# Echo a message with the timestamp
+echo "### ### ### ### ### ### ### ### ### ### ### ###   Checkpoint finished   ### ### ### ### ### ### ### ### ### ### ### ###"
+
 done
